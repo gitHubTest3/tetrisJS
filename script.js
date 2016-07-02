321dc8b9bd0ee0992cbb438c5a705edea045d2c2
gitHubTest3 <git_hub@o2.pl>
2016-07-02 13:31:23 +0200

    First working version
    Without design and ending, but at least it's working.

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-var figure;
-var bricksLeft = [];
-
 $(document).ready(function() {
-	figure = new Figure(1);
+	field = new Field(11, 20);
+	figure = new Figure(Math.floor((Math.random() * 5) + 1));
 	window.setInterval(function(){
-		//figure.lower();
+		figure.lower();
 	}, 500);
 });
-
-function lowerFigure() {
-	
-}
 
 $(document).keydown(function(e) {
    switch(e.keyCode) {
@@ -31,20 +25,79 @@ $(document).keydown(function(e) {
    }
 });
 
+var Field = function(width, height) {
+	this.width = width;
+	this.height = height;
+	this.bricksLeft = [];
+
+	var field = document.createElement('div');
+	this.field = field;
+	$(this.field).css('width', 30*width).css('height', 30*height).addClass("field").appendTo($("body"));
+};
+
+
+Field.prototype.removeFullRows = function() {
+	var bricksInRows = new Array(this.height).fill(0);//array containing amount of bricks in every row
+	
+	$.each(this.bricksLeft, function(){//for each brick in a field
+		bricksInRows[this.y]++;//increment value of bricks in a row
+	});
+	
+	for(var i in bricksInRows) {//for each row
+		if(bricksInRows[i] == this.width) {//if it's full
+			this.bricksLeft = $.map(this.bricksLeft, function(item, index){
+				if(item.y == i) {//delete brick
+					item.brick.remove();//removing "brick" div
+					return null;//doesn't return brick to an array
+				} else if(item.y < i) {//lower brick
+					item.move(0, 1);
+					return item;//returns brick to an array
+				}
+			});
+		}
+	}
+};
+
 var Figure = function(type) {
 	this.type = type;
-	if(this.type == 1)
-		this.statesAmount = 4;
-	figure = document.createElement('div');
+	var figure = document.createElement('div');
 	this.figure = figure;
-	$(figure).addClass("figure").appendTo($(".field"));
+	$(this.figure).addClass("figure").appendTo($(".field"));
+	
 	this.state = 1;
+	if(this.type == 1 || this.type == 4 || this.type == 5)
+		this.statesAmount = 4;
+	else if(this.type == 2)
+		this.statesAmount = 1;
+	else if(this.type == 3)
+		this.statesAmount = 2;
+	
 	this.bricks = [];
-	if(type==1) {
+	if(type==1) {//platform
 		this.bricks[0] = new Brick(this, 4, 0);
 		this.bricks[1] = new Brick(this, 5, 0);
 		this.bricks[2] = new Brick(this, 6, 0);
 		this.bricks[3] = new Brick(this, 5, 1);
+	} else if(type==2) {//square
+		this.bricks[0] = new Brick(this, 5, 0);
+		this.bricks[1] = new Brick(this, 6, 0);
+		this.bricks[2] = new Brick(this, 5, 1);
+		this.bricks[3] = new Brick(this, 6, 1);
+	} else if(type==3) {//line
+		this.bricks[0] = new Brick(this, 4, 0);
+		this.bricks[1] = new Brick(this, 5, 0);
+		this.bricks[2] = new Brick(this, 6, 0);
+		this.bricks[3] = new Brick(this, 7, 0);
+	} else if(type==4) {//L normal
+		this.bricks[0] = new Brick(this, 4, 0);
+		this.bricks[1] = new Brick(this, 5, 0);
+		this.bricks[2] = new Brick(this, 6, 0);
+		this.bricks[3] = new Brick(this, 4, 1);
+	} else if(type==5) {//L turned
+		this.bricks[0] = new Brick(this, 4, 0);
+		this.bricks[1] = new Brick(this, 5, 0);
+		this.bricks[2] = new Brick(this, 6, 0);
+		this.bricks[3] = new Brick(this, 6, 1);
 	}
 };
 
@@ -53,14 +106,16 @@ Figure.prototype.lower = function() {
 		figure.move(0, 1);
 	else {
 		for(var i in figure.bricks) {
-			$(figure.bricks[i].brick).appendTo($(".field"));//przeniesienie kostek z poprzedniej figury bezpośrednio na planszę
-			bricksLeft.push(figure.bricks[i]);
-			//TODO: nie zapisywanie bricksów które już są w tablicy
+			$(figure.bricks[i].brick).appendTo($(".field"));//moving bricks from previous figure straight onto field
+			field.bricksLeft.push(figure.bricks[i]);//bricks goes to an array with left bricks
 		}
 		
-		figure.figure.remove();//usunięcie diva
+		figure.figure.remove();//removing "figure" div
 		
-		figure = new Figure(1);//sprawdzanie, czy figura może się pojawić
+		field.removeFullRows();
+		
+		figure = new Figure(Math.floor((Math.random() * 5) + 1));
+		//TODO: check if figure can be created, if not - game end
 	}
 };
 
@@ -79,53 +134,109 @@ Figure.prototype.move = function(x, y) {
 };
 
 Figure.prototype.turn = function() {
-	var positions = [];
-
-	for(var i in this.bricks) {//saving positions of bricks in a figure
-		positions[i] = [];//creating 2nd dimension in array
-        positions[i]["x"] = this.bricks[i].x;
-		positions[i]["y"] = this.bricks[i].y;
+	var shifts = [];//making array for shifts of every brick in a figure
+	for(var i in this.bricks) {//creating 2nd dimension of an array
+		shifts[i] = [];
+		shifts[i]["x"] = 0;
+		shifts[i]["y"] = 0; 
 	}
 	
-	if(this.type == 1) {//counting new positions of bricks in a figure
+	if(this.type == 1) {//setting shifts of bricks in a figure
 		if(this.state == 1) {
-			positions[0]["x"] += 2;
-			positions[0]["y"] += 2;
-			positions[1]["x"] += 1;
-			positions[1]["y"] += 1;
+			shifts[0]["x"] = 2;
+			shifts[0]["y"] = 2;
+			shifts[1]["x"] = 1;
+			shifts[1]["y"] = 1;
 		} else if(this.state == 2) {
-			positions[1]["x"] -= 1;
-			positions[1]["y"] += 1;
-			positions[2]["x"] -= 2;
-			positions[2]["y"] += 2;
+			shifts[1]["x"] = -1;
+			shifts[1]["y"] = 1;
+			shifts[2]["x"] = -2;
+			shifts[2]["y"] = 2;
 		} else if(this.state == 3) {
-			positions[0]["x"] -= 2;
-			positions[0]["y"] -= 2;
-			positions[1]["x"] -= 1;
-			positions[1]["y"] -= 1;
+			shifts[0]["x"] = -2;
+			shifts[0]["y"] = -2;
+			shifts[1]["x"] = -1;
+			shifts[1]["y"] = -1;
 		} else if(this.state == 4) {
-			positions[1]["x"] += 1;
-			positions[1]["y"] -= 1;
-			positions[2]["x"] += 2;
-			positions[2]["y"] -= 2;
+			shifts[1]["x"] = 1;
+			shifts[1]["y"] = -1;
+			shifts[2]["x"] = 2;
+			shifts[2]["y"] = -2;
+		}
+	} else if(this.type == 3) {
+		if(this.state == 1) {
+			shifts[0]["x"] = 2;
+			shifts[0]["y"] = -2;
+			shifts[1]["x"] = 1;
+			shifts[1]["y"] = -1;
+			shifts[3]["x"] = -1;
+			shifts[3]["y"] = 1;
+		} else if(this.state == 2) {
+			shifts[0]["x"] = -2;
+			shifts[0]["y"] = 2;
+			shifts[1]["x"] = -1;
+			shifts[1]["y"] = 1;
+			shifts[3]["x"] = 1;
+			shifts[3]["y"] = -1;
+		}
+	} else if(this.type == 4) {
+		if(this.state == 1) {
+			shifts[0]["x"] = 2;
+			shifts[0]["y"] = 1;
+			shifts[3]["x"] = 2;
+			shifts[3]["y"] = 1;
+		} else if(this.state == 2) {
+			shifts[1]["x"] = -1;
+			shifts[1]["y"] = 2;
+			shifts[2]["x"] = -1;
+			shifts[2]["y"] = 2;
+		} else if(this.state == 3) {
+			shifts[0]["x"] = -2;
+			shifts[0]["y"] = -1;
+			shifts[3]["x"] = -2;
+			shifts[3]["y"] = -1;
+		} else if(this.state == 4) {
+			shifts[1]["x"] = 1;
+			shifts[1]["y"] = -2;
+			shifts[2]["x"] = 1;
+			shifts[2]["y"] = -2;
+		}
+	} else if(this.type == 5) {
+		if(this.state == 1) {
+			shifts[0]["x"] = 1;
+			shifts[0]["y"] = 2;
+			shifts[1]["x"] = 1;
+			shifts[1]["y"] = 2;
+		} else if(this.state == 2) {
+			shifts[2]["x"] = -2;
+			shifts[2]["y"] = 1;
+			shifts[3]["x"] = -2;
+			shifts[3]["y"] = 1;
+		} else if(this.state == 3) {
+			shifts[0]["x"] = -1;
+			shifts[0]["y"] = -2;
+			shifts[1]["x"] = -1;
+			shifts[1]["y"] = -2;
+		} else if(this.state == 4) {
+			shifts[2]["x"] = 2;
+			shifts[2]["y"] = -1;
+			shifts[3]["x"] = 2;
+			shifts[3]["y"] = -1;
 		}
 	}
-	
+
 	var collisionOccurs = false;
-	
-	if(this.type == 1) {//checking collisions of new positions
-		if(this.state == 1) {
-			for(var i in this.bricks)
-				if(this.bricks[i].checkCollisions(positions[i]["x"], positions[i]["y"]))
-					collisionOccurs = true;
-		}
+	for(var i in this.bricks) {//checking collisions
+		if(this.bricks[i].checkCollisions(shifts[i]["x"], shifts[i]["y"]))
+			collisionOccurs = true;
 	}
 	
 	if(!collisionOccurs) {//rotating figure
 		for(var i in this.bricks)
-			this.bricks[i].move(positions[i]["x"], positions[i]["y"]);
+			this.bricks[i].move(shifts[i]["x"], shifts[i]["y"]);
 		this.state++;
-		this.state = this.state % this.statesAmount;
+		if(this.state > this.statesAmount)
+			this.state -= this.statesAmount;
 	}
 };
 
@@ -144,10 +255,10 @@ Brick.prototype.checkCollisions = function(x, y) {
 	
 	var collisionOccurs = false;
 	
-	if(newX<0 || newX>10 || newY>19)//sprawdzenie krawędzi ekranu
+	if(newX<0 || newX>field.width-1 || newY<0 || newY>field.height-1)//checking field boundaries
 		collisionOccurs = true;
 
-	$.each(bricksLeft, function(){//sprawdzenie wszystkich kostek poza należącymi do figury
+	$.each(field.bricksLeft, function(){//checking collisions with bricks in field
 		if(this.x == newX && this.y == newY) {
 			collisionOccurs = true;
 		}
